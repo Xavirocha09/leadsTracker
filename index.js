21d067b6c89d310609ac9921dd020e410a87690d
javi <rocha.javier.9999@gmail.com>
2024-07-23 22:57:01 +0200

    fff

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-let myLeads = []
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js"
+import { getDatabase,
+    ref,
+    push,
+    onValue,
+    remove } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: process.env.OPENAI_API_KEY
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
 
 function render(leads) {
     let listItems = ""
@@ -33,15 +32,17 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+onValue(referenceInDB, function(snapshot){
+    const snapshotValues = snapshot.val()
+    const leads = object.values(snapshotValues)
+    render(leads)
+})
+
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    remove (referenceInDB)
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    push(referenceInDB, inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
 })
